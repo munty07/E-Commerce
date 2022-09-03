f256129b020ad0f0bbc0bbf1542ba12684cd1b8e
muntyancraciunela@gmail.com <muntyancraciunela@gmail.com>
2022-09-03 18:18:16 +0300

    modificari in urma verificarii

--- others/implementation.js
+++ others/implementation.js
@@ -1,4 +1,4 @@
-
+// PARTEA DEA AUTENTIFICARE
 
 import {getAuth, GoogleAuthProvider, signInWithRedirect} 
 from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
@@ -73,3 +73,95 @@ logBtn.addEventListener('click', function(){
         document.getElementById("error-name").style.color = "red";
     });
 })
+
+
+// PARTEA DE INREGISTRARE
+
+
+document.getElementById("registerBtn").addEventListener('click', function(){
+    if(!validate()){
+        console.log("Date invalide");
+        return
+    }
+    const name = document.getElementById("nameBox").value;
+    const username = document.getElementById("userBox").value;
+    const mail = document.getElementById("mailBox").value;
+    const phone = document.getElementById("phoneBox").value;
+    const county = document.getElementById("countyBox").value;
+    const city = document.getElementById("cityBox").value;
+    const address = document.getElementById("addressBox").value;
+    const code = document.getElementById("codeBox").value;
+    const pass = document.getElementById("passBox").value;
+
+    createUserWithEmailAndPassword(auth, mail, pass)
+    .then(() => {
+        document.getElementById("error-all").innerHTML = "Date valide!";
+        document.getElementById("error-all").style.color = "green";     
+        const dbRef = ref(db);
+        const user = auth.currentUser;
+        const uid = user.uid;
+        console.log(uid);
+        
+        get(child(dbRef, "Conturi/" + uid)).then((snapshot) => {
+            if(!snapshot.exists()){
+                set(ref(db, "Conturi/" + uid),{
+                    Nume: name,
+                    Utilizator: username,
+                    Mail: mail,
+                    Telefon: phone,
+                    Judet: county,
+                    Oras: city,
+                    Adresa: address,
+                    Cod_postal: code,
+                    Parola: encPass(),
+                    Rol: "user"
+                })
+                .then(() => {
+                    alert("Contul a fost creat cu succes!");
+                    signOut(auth)
+                    .then(()=>{
+                        console.log("Utilizatorul a fost deconectat");
+                        window.location.replace("form_login.html");
+                    })
+                    .catch((error)=>{
+                        console.log(error);
+                    })
+                })
+                .catch((error) => {
+                    alert("Eroare: " + error);
+                })
+            }
+        });
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        document.getElementById("error-all").innerHTML = "Exista deja un cont cu aceasta adresa de mail!";
+        document.getElementById("error-all").style.color = "red";
+        
+        console.log(errorCode + errorMessage);
+    });
+})
+
+//functia de criptare a parolei
+function encPass(){
+    var psw = document.getElementById("passBox");
+    var password = CryptoJS.AES.encrypt(psw.value, psw.value);
+    // returneaza parola criptata
+    return password.toString();
+}
+
+
+// PARTEA DE RESETARE A PAROLEI
+
+sendPasswordResetEmail(auth, email)
+.then(function(){
+    document.getElementById("error-mail").innerHTML = "Mail-ul a fost trimis cu succes!";
+    document.getElementById("error-mail").style.color = "green";
+    setTimeout(redirectPage, 1000);
+})
+.catch(() => {
+
+    document.getElementById("error-mail").innerHTML = "Nu exista cont cu aceasta adresa de mail!";
+    document.getElementById("error-mail").style.color = "red";
+});
--- script_Register.js
+++ script_Register.js
@@ -111,84 +111,72 @@ function validate(){
     return true;
 }
 
-////////////////
+// EVENIMENT PENTRU BUTONUL DE INREGISTRARE 
 document.getElementById("registerBtn").addEventListener('click', function(){
     if(!validate()){
         console.log("Date invalide");
         return
     }
+    const name = document.getElementById("nameBox").value;
+    const username = document.getElementById("userBox").value;
+    const mail = document.getElementById("mailBox").value;
+    const phone = document.getElementById("phoneBox").value;
+    const county = document.getElementById("countyBox").value;
+    const city = document.getElementById("cityBox").value;
+    const address = document.getElementById("addressBox").value;
+    const code = document.getElementById("codeBox").value;
+    const pass = document.getElementById("passBox").value;
+
+    createUserWithEmailAndPassword(auth, mail, pass)
+    .then(() => {
+        document.getElementById("error-all").innerHTML = "Date valide!";
+        document.getElementById("error-all").style.color = "green";
+        console.log("Date valide");
         
-        const name = document.getElementById("nameBox").value;
-        const username = document.getElementById("userBox").value;
-        const mail = document.getElementById("mailBox").value;
-        const phone = document.getElementById("phoneBox").value;
-        const county = document.getElementById("countyBox").value;
-        const city = document.getElementById("cityBox").value;
-        const address = document.getElementById("addressBox").value;
-        const code = document.getElementById("codeBox").value;
-        const pass = document.getElementById("passBox").value;
-        const pass2 = document.getElementById("pass2Box").value;
-
-        createUserWithEmailAndPassword(auth, mail, pass)
-        .then((userCredential) => {
-            document.getElementById("error-all").innerHTML = "Date valide!";
-            document.getElementById("error-all").style.color = "green";
-            console.log("Date valide");
-            
-            const dbRef = ref(db);
-            // Signed in 
-            const user = auth.currentUser;//getCurrentUser();//userCredential.user;
-            console.log("creat");
-            console.log(user);
-
-            const uid = user.uid;
-            console.log(uid);
-            
-
-            get(child(dbRef, "Conturi/" + uid)).then((snapshot) => {
-                if(snapshot.exists()){
-                    alert("Utilizatorul exista deja!");
-                }else{
-                    set(ref(db, "Conturi/" + uid),{
-                        Nume: name,
-                        Utilizator: username,
-                        Mail: mail,
-                        Telefon: phone,
-                        Judet: county,
-                        Oras: city,
-                        Adresa: address,
-                        Cod_postal: code,
-                        Parola: encPass(),
-                        Rol: "user"
+        const dbRef = ref(db);
+        const user = auth.currentUser;
+        const uid = user.uid;
+        console.log(uid);
+        
+        get(child(dbRef, "Conturi/" + uid)).then((snapshot) => {
+            if(!snapshot.exists()){
+                set(ref(db, "Conturi/" + uid),{
+                    Nume: name,
+                    Utilizator: username,
+                    Mail: mail,
+                    Telefon: phone,
+                    Judet: county,
+                    Oras: city,
+                    Adresa: address,
+                    Cod_postal: code,
+                    Parola: encPass(),
+                    Rol: "user"
+                })
+                .then(() => {
+                    alert("Contul a fost creat cu succes!");
+                    signOut(auth)
+                    .then(()=>{
+                        console.log("Utilizatorul a fost deconectat");
+                        window.location.replace("form_login.html");
                     })
-                    .then(() => {
-                        alert("Contul a fost creat cu succes!");
-                        ////////////
-                        signOut(auth)
-                        .then(()=>{
-                            console.log("Utilizatorul a fost deconectat");
-                            window.location.replace("form_login.html");
-                        })
-                        .catch((error)=>{
-                            console.log(error);
-                        })
-                        ///////////
+                    .catch((error)=>{
+                        console.log(error);
                     })
-                    .catch((error) => {
-                        alert("Eroare: " + error);
-                    })
-                }
-            });
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            document.getElementById("error-all").innerHTML = "Exista deja un cont cu aceasta adresa de mail!";
-            document.getElementById("error-all").style.color = "red";
-            
-            console.log(errorCode + errorMessage);
+                })
+                .catch((error) => {
+                    alert("Eroare: " + error);
+                })
+            }
         });
-    
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        document.getElementById("error-all").innerHTML = "Exista deja un cont cu aceasta adresa de mail!";
+        document.getElementById("error-all").style.color = "red";
+        
+        console.log(errorCode + errorMessage);
+    });
 })
 
 //functia de criptare a parolei
